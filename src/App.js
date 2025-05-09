0a410f5215c983d5b4ba5a8e3f2839988838fa92
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-05-09 20:38:38 +0530

    Updating title and favicon based on game type

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import WordleGame from './components/Wordle/Game';
 import ColoraleGame from './components/Colorale/Game';
@@ -7,6 +7,23 @@ import './App.css';
 
 function App() {
   const [gameType, setGameType] = useState('wordle');
+
+  useEffect(() => {
+    // Dynamically update the document title
+    document.title = gameType === 'wordle' ? 'WORDLE' : 'COLORALE';
+
+    // Dynamically update the favicon
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.href = gameType === 'wordle' ? `${process.env.PUBLIC_URL}/favicon-wordle.ico` : `${process.env.PUBLIC_URL}/favicon-colorale.ico`;
+    }
+
+    const appleTouchIcon = document.querySelector("link[rel='apple-touch-icon']");
+    if (appleTouchIcon) {
+      console.log(appleTouchIcon)
+      appleTouchIcon.href = gameType === 'wordle' ? `${process.env.PUBLIC_URL}/apple-touch-icon-wordle.png` : `${process.env.PUBLIC_URL}/apple-touch-icon-colorale.png`;
+    }
+  }, [gameType]);
 
   return (
     <div className={`app ${gameType}`}>
